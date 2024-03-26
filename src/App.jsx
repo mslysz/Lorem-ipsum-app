790556798b4a3a6ec838f61f460a2cd37ec42a6e
mslysz <macslysz@gmail.com>
2024-03-26 06:06:43 +0000

    I added a handleChange that updates the current State - the number that is selected in the input

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
 
   const handleChange = (e) => {
     e.preventDefault();
+    setCount(e.target.value);
   };
-
+  console.log(count);
   return (
     <div className='section-center'>
       <h4 className='title'>tired of boring lorem ipsum?</h4>
@@ -18,6 +19,7 @@ const App = () => {
           className='form-input'
           id='number'
           onChange={handleChange}
+          value={count}
           min={1}
           max={8}
         />
